7b0a1aa734229f6d1a2d1918afad55edcda083ef
brundaI <brundavishishta199913@gmail.com>
2021-03-10 11:13:10 +0530

    changing again mar10

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
-fmt.Println("Hello World")
-fmt.Println("First change local")
-fmt.Println("Second change local")
-  fmt.Println("Third local change")
-  fmt.Println("Practising")
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("First change local")
+	fmt.Println("Second change local")
+	fmt.Println("Third local change")
+	fmt.Println("Practising")
+	fmt.Println("change mar10")
 }
